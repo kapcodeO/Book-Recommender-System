f6ce035806bc4293d6bfab1b678f389ff5917470
kapcodeO <kapilojha953@gmail.com>
2025-07-04 01:31:06 +0530

    added zip file to data folder

---
+++ book_recommender/constant/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+ROOT_DIR = os.getcwd()
+
+# main config file path
+CONFIG_DIR = "config"
+CONFIG_FILE = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, CONFIG_FILE)
