380a566bdee4fee0ac0495b55148b6a785a7b1ad
kapcodeO <kapilojha953@gmail.com>
2025-07-06 12:51:42 +0530

    updated config_entity.py

--- book_recommender/entity/config_entity.py
+++ book_recommender/entity/config_entity.py
@@ -9,3 +9,6 @@ DataValidationConfig = namedtuple("DataValidationConfig",
 
 DataTransformationConfig = namedtuple("DataTransformationConfig",
                                       ["clean_data_dir", "transformed_data_dir"])
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig",
+                                ["transformed_data_dir", "trained_model_dir", "trained_model_name"])
