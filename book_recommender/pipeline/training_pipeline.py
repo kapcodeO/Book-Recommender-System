96630db36c770c9e12a99476857a0745ef869af8
kapcodeO <kapilojha953@gmail.com>
2025-07-06 19:27:49 +0530

    updated training_pipeline.py

--- book_recommender/pipeline/training_pipeline.py
+++ book_recommender/pipeline/training_pipeline.py
@@ -1,12 +1,14 @@
 from book_recommender.components.data_ingestion import DataIngestion
 from book_recommender.components.data_validation import DataValidation
 from book_recommender.components.data_transformation import DataTransformation
+from book_recommender.components.model_trainer import ModelTrainer
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
         self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     def start_training_pipeline(self):
         """
@@ -16,3 +18,4 @@ class TrainingPipeline:
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
         self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_training()
